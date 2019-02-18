18a0ecc3f2631207b4f4a84959f71681ecb13150
liaoxianghao <lxhao@admindeMacBook-Pro-23.local>
2019-02-18 21:11:20 +0800

    开启历史任务查看服务

--- startHadoop.sh
+++ startHadoop.sh
@@ -9,13 +9,16 @@ do
     if [ ${hadoop_arr[$m]} = "master" ]
     then
         echo "master"
-        docker run -tid --name ${hadoop_arr[$m]} -h ${hadoop_arr[m]} --add-host hadoop0:100.10.0.100 --add-host hadoop1:100.10.0.101 --add-host hadoop2:100.10.0.102 --net=hadoop --ip=${hadoop_ip[m]} -p 8088:8088 -p 50070:50070 hadoop:2.9
+        docker run -tid --name ${hadoop_arr[$m]} -h ${hadoop_arr[m]} --add-host master:100.10.0.100 --add-host slave1:100.10.0.101 --add-host slave2:100.10.0.102 --net=hadoop --ip=${hadoop_ip[m]} -p 8088:8088 -p 50070:50070 hadoop:2.9
     else
-        docker run -tid --name ${hadoop_arr[$m]} -h ${hadoop_arr[m]} --add-host hadoop0:100.10.0.100 --add-host hadoop1:100.10.0.101 --add-host hadoop2:100.10.0.102 --net=hadoop --ip=${hadoop_ip[m]} hadoop:2.9
+        docker run -tid --name ${hadoop_arr[$m]} -h ${hadoop_arr[m]} --add-host master:100.10.0.100 --add-host slave1:100.10.0.101 --add-host slave2:100.10.0.102 --net=hadoop --ip=${hadoop_ip[m]} hadoop:2.9
     fi
 done;
 
 # format hdfs
 docker exec ${hadoop_arr[0]} /usr/local/hadoop/bin/hdfs namenode -format
+docker exec ${hadoop_arr[0]} /usr/local/hadoop/bin/mr-jobhistory-daemon.sh start historyserver
 # 启动hadoop集群
 docker exec ${hadoop_arr[0]} /root/startHadoop.exp
+
+
